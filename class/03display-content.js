98874da2e63544eb2c0b8694dc3d2c04dcb289c5
Sakshee suthar <139334983+Sakshee9242@users.noreply.github.com>
2023-09-26 21:15:26 +0530

    25

---
+++ class/03display-content.js
@@ -0,0 +1,10 @@
+/*
+ // document.write("BODY")
+document.getElementById('xyz').innerHTML = "DIV"
+*/
+
+window.alert("window alert..!");
+window.confirm("are you sure?");
+window.prompt("5 + 5 = ? ");
+
+window.print();
